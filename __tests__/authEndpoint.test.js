80299a9350f3aaf014d76edc040719e0b79bf415
Sean Hockin <sean.hockin@gmail.com>
2020-02-24 16:26:27 -0700

    Implemented end points and middleware

---
+++ __tests__/authEndpoint.test.js
@@ -0,0 +1,13 @@
+test.todo('add stuff');
+// const request = require('supertest');
+// const server = require('../api/server');
+
+// describe('auth endpoint', () => {
+//     describe('registration', () => {
+//         it('')
+//     })
+
+//     describe('login', () => {
+
+//     })
+// })
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,61 @@
 const router = require('express').Router();
+const Users = require('./usersModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let {username, password} = req.body;
+
+  if(username && password) {
+
+    password = bcrypt.hashSync(password, 10);
+
+    Users.addUser({username, password})
+      .then(user => {
+        [user] = user;
+        res.status(200).json(user);
+      })
+      .catch(err => res.status(500).json({message: `A server error occured adding user: ${err}`}))
+  } else {
+    res.status(400).json({message: 'You must provide a username and password'});
+  }
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  const {username, password} = req.body;
+
+  if(username && password) {
+    Users.getUserBy({username}).first()
+      .then(user => {
+        console.log(user)
+        if(bcrypt.compareSync(password, user.password)) {
+          const token = genToken(user);
+          res.status(200).json({message: `Welcome ${user.username}`, token: token});
+        } else {
+          res.status(403).json({message: 'Invalid credentials'});
+        }
+      })
+  } else {
+    res.status(400).json({message: 'You must provide a username and password'});
+  }
+
+
 });
+
+function genToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: '4h'
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
